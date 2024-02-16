09be2e1ebb007ed78fe373278d9035eb306aafe9
choseenonee <okaymans44@gmail.com>
2024-02-16 16:21:25 +0300

    add: full image blur detection

--- blur_service/main.cpp
+++ blur_service/main.cpp
@@ -4,6 +4,23 @@
 
 #include <opencv2/opencv.hpp>
 
+bool IsBlurry(const cv::Mat& gray) {
+    //Cool, let's compute the laplacian of the gray image:
+    cv::Mat laplacianImage;
+    cv::Laplacian( gray, laplacianImage, CV_64F );
+
+    //Prepare to compute the mean and standard deviation of the laplacian:
+    cv::Scalar mean, stddev;
+    cv::meanStdDev( laplacianImage, mean, stddev, cv::Mat() );
+
+    //Let’s compute the variance:
+    double variance = stddev.val[0] * stddev.val[0];
+
+    double blurThreshold = 300;
+
+    return variance <= blurThreshold;
+}
+
 
 int main() {
 //    путь выбирается относительно CMake IDE, то есть в данном случае, наша "отправная точка" - cmake-build-debug (CLion)
@@ -11,7 +28,7 @@ int main() {
 //    НО ЭТО НИКАК НЕ ОТРАЖАЕТСЯ НА imwrite, только на то, что namespace cv::samples::
     cv::samples::addSamplesDataSearchPath("/Users/thechosenone/CV-photo-analyzer/blur_service/src/bad");
 
-    std::string image_path = cv::samples::findFile("Копия photo2.jpg");
+    std::string image_path = cv::samples::findFile("BLURRY.png");
     cv::Mat img = imread(image_path, cv::IMREAD_GRAYSCALE);
     // IMREAD_COLOR loads the image in the BGR 8-bit format. This is the default that is used here.
     // IMREAD_UNCHANGED loads the image as is (including the alpha channel if present)
@@ -23,12 +40,13 @@ int main() {
         return 1;
     }
 
-    cv::imshow("Display window", img);
+//    cv::imshow("Display window", img);
+//
+//    int k = cv::waitKey(0); // Wait for a keystroke in the window
+//    if(k == 's')
+//    {
+//        cv::imwrite("starry_night.png", img);
+//    }
 
-    int k = cv::waitKey(0); // Wait for a keystroke in the window
-    if(k == 's')
-    {
-        cv::imwrite("starry_night.png", img);
-    }
-
+    std::cout << IsBlurry(img);
 }
